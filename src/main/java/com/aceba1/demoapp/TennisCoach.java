b81a603df8d11bc9aae3ac13d1e5c5518ca32844
Arniel <aceba1@outlook.com>
2020-10-27 17:42:52 -0400

    Add PostConstruct and PreDestroy

--- src/main/java/com/aceba1/demoapp/TennisCoach.java
+++ src/main/java/com/aceba1/demoapp/TennisCoach.java
@@ -5,8 +5,10 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
-@Scope("prototype")
 public class TennisCoach implements Coach {
 
   private FortuneService service;
@@ -29,6 +31,16 @@ public class TennisCoach implements Coach {
   public void setFortuneService(FortuneService fortuneService) {
     System.out.println(">> TennisCoach : Do My Bidding");
     service = fortuneService;
+  }
+
+  @PostConstruct
+  private void onStartup() {
+    System.out.println(">> TennisCoach : On Startup");
+  }
+
+  @PreDestroy
+  private void onCleanup() {
+    System.out.println(">> TennisCoach : On Cleanup");
   }
 
   @Override
